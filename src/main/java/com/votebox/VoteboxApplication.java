cfc77e1dbfbd34aa019bf4e6d8e61de247aa867a
navneetraja <nav11raja@gmail.com>
2020-08-13 07:39:59 +0530

    Initializr code

---
+++ src/main/java/com/votebox/VoteboxApplication.java
@@ -0,0 +1,13 @@
+package com.votebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/votebox/VoteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
